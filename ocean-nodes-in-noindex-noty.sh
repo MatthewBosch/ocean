d1ebc2cedd4bac70aa9f8eb947e27771d28ac52a
MatthewBosch <152050466+MatthewBosch@users.noreply.github.com>
2025-01-06 13:33:17 +0800

    Update ocean-nodes-in-noindex-noty.sh

--- ocean-nodes-in-noindex-noty.sh
+++ ocean-nodes-in-noindex-noty.sh
@@ -3,13 +3,12 @@
 # 清屏
 clear
 
-# 获取公网 IPv4 地址
-public_ipv4=$(curl -4 -s https://ifconfig.me)
-if [[ -z "$public_ipv4" ]]; then
-    echo "无法获取公网 IPv4 地址，请检查网络连接。"
+# 获取公网 IP 地址作为默认值
+default_ip=$(curl -4 -s https://ifconfig.me)
+if [[ -z "$default_ip" ]]; then
+    echo "无法获取公网 IP 地址，请检查网络连接。"
     exit 1
 fi
-echo "自动获取的公网 IP 地址为: $public_ipv4"
 
 # 询问需要生成的 yml 文件数量
 read -p "请输入需要生成的 yml 文件数量: " yml_count
@@ -17,9 +16,9 @@ read -p "请输入需要生成的 yml 文件数量: " yml_count
 # 询问容器编号的起始值
 read -p "请输入容器编号的起始值（例如，如果输入3，则容器将从 ocean-node-3 开始）: " start_index
 
-# 接收 IP 地址
-read -p "请输入 P2P 绑定的 IP 地址（默认: $public_ipv4）: " ip_address
-ip_address=${ip_address:-$public_ipv4}
+# 接收 P2P 绑定的公网 IP 地址，默认值为 `default_ip`
+read -p "请输入 P2P 绑定的 IP 地址（默认: $default_ip）: " ip_address
+ip_address=${ip_address:-$default_ip}
 
 # 接收 Infura Project ID
 read -p "请输入 Infura Project ID (例如：5d9f50e145964c318dac0d6526278993): " infura_id
@@ -99,10 +98,23 @@ services:
       - "$p2p_ipv6_ws_port:$p2p_ipv6_ws_port"
     environment:
       PRIVATE_KEY: '${wallets[$((i + 1))]#*, Private Key: }'
-      RPCS: '{"1":{"rpc":"https://mainnet.infura.io/v3/$infura_id","chainId":1},"10":{"rpc":"https://optimism-mainnet.infura.io/v3/$infura_id","chainId":10}}'
+      RPCS: '{"1":{"rpc":"https://mainnet.infura.io/v3/$infura_id","fallbackRPCs":["https://rpc.ankr.com/eth","https://1rpc.io/eth","https://eth.api.onfinality.io/public"],"chainId":1,"network":"mainnet","chunkSize":100},"10":{"rpc":"https://optimism-mainnet.infura.io/v3/$infura_id","fallbackRPCs":["https://optimism-mainnet.public.blastapi.io","https://rpc.ankr.com/optimism","https://optimism-rpc.publicnode.com"],"chainId":10,"network":"optimism","chunkSize":100},"137":{"rpc":"https://polygon-mainnet.infura.io/v3/$infura_id","fallbackRPCs":["https://polygon-mainnet.public.blastapi.io","https://1rpc.io/matic","https://rpc.ankr.com/polygon"],"chainId":137,"network":"polygon","chunkSize":100},"23294":{"rpc":"https://sapphire.oasis.io","fallbackRPCs":["https://1rpc.io/oasis/sapphire"],"chainId":23294,"network":"sapphire","chunkSize":100},"23295":{"rpc":"https://testnet.sapphire.oasis.io","chainId":23295,"network":"sapphire-testnet","chunkSize":100},"11155111":{"rpc":"https://sepolia.infura.io/v3/$infura_id","fallbackRPCs":["https://1rpc.io/sepolia","https://eth-sepolia.g.alchemy.com/v2/demo"],"chainId":11155111,"network":"sepolia","chunkSize":100},"11155420":{"rpc":"https://optimism-sepolia.infura.io/v3/$infura_id","fallbackRPCs":["https://endpoints.omniatech.io/v1/op/sepolia/public","https://optimism-sepolia.blockpi.network/v1/rpc/public"],"chainId":11155420,"network":"optimism-sepolia","chunkSize":100}}'
+      DB_URL: 'http://typesense:8108/?apiKey=xyz'
+      IPFS_GATEWAY: 'https://ipfs.io/'
+      ARWEAVE_GATEWAY: 'https://arweave.net/'
       INTERFACES: '["HTTP","P2P"]'
       ALLOWED_ADMINS: '["$evm_address"]'
+      INDEXER_NETWORKS: '[]'
+      DASHBOARD: 'true'
       HTTP_API_PORT: '$ocean_http_port'
+      P2P_ENABLE_IPV4: 'true'
+      P2P_ENABLE_IPV6: 'false'
+      P2P_ipV4BindAddress: '0.0.0.0'
+      P2P_ipV4BindTcpPort: '$p2p_ipv4_tcp_port'
+      P2P_ipV4BindWsPort: '$p2p_ipv4_ws_port'
+      P2P_ipV6BindAddress: '::'
+      P2P_ipV6BindTcpPort: '$p2p_ipv6_tcp_port'
+      P2P_ipV6BindWsPort: '$p2p_ipv6_ws_port'
       P2P_ANNOUNCE_ADDRESSES: '["/ip4/$ip_address/tcp/$p2p_ipv4_tcp_port", "/ip4/$ip_address/ws/tcp/$p2p_ipv4_ws_port"]'
 EOL
 
@@ -114,11 +126,22 @@ while true; do
   read -p "是否执行生成的 yml 文件？(yes/no): " execute_choice
   case $execute_choice in
     [Yy]* )
+      # 检查 docker compose 或 docker-compose
+      if command -v docker compose &> /dev/null; then
+          docker_cmd="docker compose"
+      elif command -v docker-compose &> /dev/null; then
+          docker_cmd="docker-compose"
+      else
+          echo "未检测到 docker compose 或 docker-compose，无法继续执行。"
+          exit 1
+      fi
+
       for ((i = 0; i < yml_count; i++)); do
         current_index=$((start_index + i))
         folder="ocean$current_index"
         cd $folder
-        docker-compose up -d
+        echo "正在使用 $docker_cmd up -d 在文件夹: $folder"
+        $docker_cmd up -d
         cd ..
       done
       echo "所有 yml 文件已执行完毕。"
